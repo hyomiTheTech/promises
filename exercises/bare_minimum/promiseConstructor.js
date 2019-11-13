a2cb829cd9e0b1e7a86ab66b6d4da30a0500e17d
Eui Hyun Kim <hyomithetech@gmail.com>
2019-11-12 19:14:29 -0800

    Complete promiseConstructor

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -4,17 +4,36 @@
  * to the function invocation, while errors should be available in the `catch` block
  */
 
-var fs = require('fs');
-var request = require('request');
-var Promise = require('bluebird');
+var fs = require("fs");
+var request = require("request");
+var Promise = require("bluebird");
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        var stringifiedData = data.toString();
+        var dataArray = stringifiedData.split("\n");
+        resolve(dataArray[0]);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+  });
   // TODO
 };
 
